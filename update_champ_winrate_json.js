a38b894b898908a4d7bb1a6017ea2529a0ec7d30
alexo75 <alexander.oneill75@gmail.com>
2024-01-23 23:52:36 -0500

    addint a function
    this will do the actual updating of the champion win rate, and (I guess) it will attach this information to the json to be displayed in the winrates later.

---
+++ update_champ_winrate_json.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+// the goal of this is to make a funciton 
+// which will get/update champion information
+// probably will run once a day
+
+
+const update_champ_winrate_json = () => {
+  return (
+    <div>update_champ_winrate_json</div>
+  )
+}
+
+export default update_champ_winrate_json
+
